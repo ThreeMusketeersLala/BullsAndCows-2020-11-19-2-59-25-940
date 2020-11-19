85500f687a21c0c6fd5bf83386222f01f3475a8b
Ying Fu <YFu3@slb.com>
2020-11-19 15:54:43 +0800

    feat: pass test 0A0B

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,16 +5,23 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private readonly string secret = string.Empty;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            this.secret = this.secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            return this.Compare(this.secret, guess);
+        }
+
+        private string Compare(string secret, string guess)
+        {
+            return "0A0B";
         }
     }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -8,10 +8,33 @@ namespace BullsAndCowsTest
         [Fact]
         public void Should_create_BullsAndCowsGame()
         {
-            var secretGenerator = new SecretGenerator();
+            // var secretGenerator = new SecretGenerator();
+            var secretGenerator = new TestSecretGenerator();
             var game = new BullsAndCowsGame(secretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void ShouldReturn0A0BGivenAllDigitAndWrongPosition()
+        {
+            // given
+            // var secretGenerator = new SecretGenerator();
+            var secretGenerator = new TestSecretGenerator();
+            var game = new BullsAndCowsGame(secretGenerator);
+
+            // when
+            string answer = game.Guess("5678");
+            // then
+            Assert.Equal("0A0B", answer);
+        }
+    }
+
+    public class TestSecretGenerator : SecretGenerator
+    {
+        public override string GenerateSecret()
+        {
+            return "1234";
         }
     }
 }
